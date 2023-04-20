a2c91afa257371ac94c7e583b66abd1eede860d9
Daniel Armbruster <dani.armbruster@gmail.com>
2023-04-20 13:45:25 +0200

    Update `build.rs`
    Drop git dependency and rely on the submodule (see also
    https://kornel.ski/rust-sys-crate#source). Adopt `build.rs` according to
    the `bindgen` documentation.

--- build.rs
+++ build.rs
@@ -1,62 +1,45 @@
 extern crate bindgen;
-extern crate git2;
 
-use git2::Repository;
 use std::env;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::process::Command;
 
-const REPO_URL: &str = "https://github.com/iris-edu/libmseed";
-const BUILD_DIR: &str = "src/libmseed";
-const GIT_REF: &str = "refs/remotes/origin/master"; // Use the most recent version 3.x
+use bindgen::CargoCallbacks;
 
-fn fetch_libmseed(dir: &str) {
-    println!("fetching libmseed");
-    let repo = match Repository::clone(REPO_URL, dir) {
-        Ok(creepo) => creepo,
-        Err(e) => panic!("Failed to update/clone repo: {}", e),
-    };
-
-    let branch = repo.revparse_single(GIT_REF).unwrap();
-    repo.reset(&branch, git2::ResetType::Hard, None).unwrap();
-}
-
-fn make_libmseed(dir: &str) {
-    let path = std::fs::canonicalize(dir).unwrap();
-    let _ok = env::set_current_dir(&path).is_ok();
-
-    let _output = Command::new("make").output().expect("make failed");
-    let path = std::fs::canonicalize("../..").unwrap();
-    let _ok = env::set_current_dir(&path).is_ok();
-}
+const LIB_DIR: &str = "vendor";
 
 fn main() {
-    if !Path::new(BUILD_DIR).is_dir() {
-        fetch_libmseed(BUILD_DIR);
+    let package_dir_path = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap())
+        .canonicalize()
+        .expect("cannot canonicalize path");
+
+    let lib_dir_path = package_dir_path.join(LIB_DIR);
+    let lib_dir_path_str = lib_dir_path.to_str().expect("path is not a valid string");
+
+    // build library
+    env::set_current_dir(&lib_dir_path).expect("failed to change directory");
+    Command::new("make")
+        .output()
+        .expect("failed to build library");
+    env::set_current_dir(&package_dir_path).expect("failed to change directory");
+
+    let headers_path = lib_dir_path.join("libmseed.h");
+    if !headers_path.try_exists().is_ok() {
+        panic!("header file does not exist");
     }
-
-    make_libmseed(BUILD_DIR);
-
-    let search_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let search_dir = Path::new(&search_dir).join("src").join("libmseed");
+    let headers_path_str = headers_path.to_str().expect("path is not a valid string");
 
     println!("cargo:rustc-link-lib=static=mseed");
-    println!("cargo:rustc-link-search=native={}", search_dir.display());
-    println!("Searching for libraries at: {}", search_dir.display());
+    println!("cargo:rustc-link-search=native={}", lib_dir_path_str);
+    println!("cargo:rerun-if-changed={}", headers_path_str);
+
+    println!("Searching for libraries at: {}", lib_dir_path_str);
     println!("Generate bindings.rs");
 
-    let path = env::current_dir().unwrap();
-    println!("The current directory is {}", path.display());
-
-    let path: PathBuf = [BUILD_DIR, "libmseed.h"].iter().collect();
-    if !path.exists() {
-        panic!("libmseed header file: libmseed.h does not exist");
-    }
-
     let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .clang_arg("-I./src/libmseed")
-        .clang_arg("-I../libmseed")
+        .header(headers_path_str)
+        .parse_callbacks(Box::new(CargoCallbacks))
+        .clang_arg(&format!("-I{}", lib_dir_path_str))
         .allowlist_type("MS.*")
         .allowlist_var("MS_.*")
         .allowlist_var("HPT.*")
@@ -70,11 +53,10 @@ fn main() {
         .allowlist_function("msr3_.*")
         .generate()
         .expect("Unable to generate bindings");
-    println!("version: {:?}", bindgen::clang_version());
-    println!("Path");
+    // println!("version: {:?}", bindgen::clang_version());
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("couldn't write bindings");
+        .write_to_file(out_path)
+        .expect("Couldn't write bindings!");
 }
